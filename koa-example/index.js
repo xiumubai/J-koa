aa6798fd9247011c5aa89f2b75aa85cd67691e12
郭建利 <guojianli@ihappy.net.cn>
2020-06-20 16:55:38 +0800

    add：模拟请求耗时

--- koa-example/index.js
+++ koa-example/index.js
@@ -1,10 +1,19 @@
 const Koa = require('koa')
 const app = new Koa()
 
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const end = Date.now()
+  console.log(`请求${ctx.url} 耗时${parseInt(end - start)}ms`)
+})
+
 app.use(async (ctx) => {
-  ctx.body = {
-    name: 'tom',
-  }
+  const expire = Date.now() + 102
+  while (Date.now() < expire)
+    ctx.body = {
+      name: 'tom',
+    }
 })
 
 app.listen(3000)

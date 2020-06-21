fcbb07f0e1ec4009a82511065653541be19ec7dd
郭建利 <guojianli@ihappy.net.cn>
2020-06-21 18:08:32 +0800

    add: koa-router

--- koa-code/index.js
+++ koa-code/index.js
@@ -1,15 +1,57 @@
 const Koa = require('./koa')
+const Router = require('./router')
 const app = new Koa()
+const router = new Router()
 // app.use((req, res) => {
 //   console.log(req, res)
 //   res.writeHead(200)
 //   res.end('Hi koa!')
 // })
 
-app.use((ctx) => {
-  console.log(ctx.body)
-  ctx.body = 'Hi koa!'
+// app.use((ctx) => {
+//   console.log(ctx.body)
+//   ctx.body = 'Hi koa!'
+// })
+
+// function delay() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve()
+//     }, 300)
+//   })
+// }
+
+// app.use(async (ctx, next) => {
+//   ctx.body = '1'
+//   await next()
+//   ctx.body += '5'
+// })
+
+// app.use(async (ctx, next) => {
+//   ctx.body += '2'
+//   await delay()
+//   await next()
+//   ctx.body += '4'
+// })
+// app.use(async (ctx, next) => {
+//   ctx.body += '3'
+// })
+
+router.get('/index', async (ctx) => {
+  console.log('index,xx')
+  ctx.body = 'index page'
 })
+router.get('/post', async (ctx) => {
+  ctx.body = 'post page'
+})
+router.get('/list', async (ctx) => {
+  ctx.body = 'list page'
+})
+router.post('/index', async (ctx) => {
+  ctx.body = 'post page'
+})
+
+app.use(router.routes())
 
 app.listen(3000, () => {
   console.log('app server is running in 3000!')
--- koa-code/koa.js
+++ koa-code/koa.js
@@ -4,17 +4,33 @@ const response = require('./response')
 const context = require('./context')
 
 class Koa {
+  constructor() {
+    // 初始化中间件列表
+    this.middlewares = []
+  }
   listen(...args) {
-    const server = http.createServer((req, res) => {
+    const server = http.createServer(async (req, res) => {
       // this.callback(req, res)
+
+      // 创建上下文
       const ctx = this.createContext(req, res)
-      this.callback(ctx)
+      // this.callback(ctx)
+      // 组合中间件
+      const fn = this.compose(this.middlewares)
+      await fn(ctx)
+
       res.end(ctx.body)
     })
     server.listen(...args)
   }
-  use(callback) {
-    this.callback = callback
+
+  // use(callback) {
+  //   this.callback = callback
+  // }
+
+  use(middleware) {
+    // 将中间件添加到列表中
+    this.middlewares.push(middleware)
   }
 
   // 创建上下文
@@ -28,6 +44,26 @@ class Koa {
     ctx.res = ctx.response.res = res
 
     return ctx
+  }
+
+  // 添加compose
+  compose(middlewares) {
+    return function (ctx) {
+      // 传入ctx上下文
+      return dispatch(0)
+      function dispatch(i) {
+        let fn = middlewares[i]
+        if (!fn) {
+          return Promise.resolve()
+        }
+        return Promise.resolve(
+          // 将上下⽂传⼊中间件，mid(ctx,next)
+          fn(ctx, function next() {
+            return dispatch(i + 1)
+          })
+        )
+      }
+    }
   }
 }
 
